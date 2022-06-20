6af9fb64ad72802e6d61297e311bb89da08ce4c1
Juan Guerra <juan.guerra@nybblegroup.com>
2022-06-20 18:47:11 -0300

    Cambio de env para los eventos

--- TP2-CODIGO/app/src/main/java/com/l1/tp_2/entities/Event.java
+++ TP2-CODIGO/app/src/main/java/com/l1/tp_2/entities/Event.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class Event {
-    private Env env = Env.DEV;
+    private Env env = Env.PROD;
     private TypeEvent typeEvents;
     private String description;
 
